060705519417ad98458d40664ec632b9604e9fe9
limfiq <mtaufiq39@gmail.com>
2025-11-03 09:20:00 +0700

    Update 3 Nopember 2025

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function About() {
+    return (
+        <div className="bg-gray-50 text-gray-800">
+            <Header />
+            <main>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptate eius ex, in neque inventore aperiam reprehenderit incidunt qui? Explicabo nemo consequatur aperiam laboriosam asperiores animi. Laudantium ad optio quia?</p>
+            </main>
+            <Footer />
+        </div>
+    );
+}
