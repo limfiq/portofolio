f95debe1cff0ff26fa611cefccd08a3ad38a4298
limfiq <mtaufiq39@gamil.com>
2025-11-03 16:43:05 +0700

    update export halaman

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -7,7 +7,7 @@ export default function Project() {
         <div className="bg-gray-50 text-gray-800">
             <Header />
             <main>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptate eius ex, in neque inventore aperiam reprehenderit incidunt qui? Explicabo nemo consequatur aperiam laboriosam asperiores animi. Laudantium ad optio quia?</p>
+                <RecentProject />
             </main>
             <Footer />
         </div>
