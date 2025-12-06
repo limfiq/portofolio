c0ad072e3bf85374992b577fcfba769049cab1ae
limfiq <mtaufiq39@gmail.com>
2025-12-06 12:06:08 +0700

    Fix Supabase server env runtime

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { createSupabaseServerClient } from '@/utils/supabaseClient';
+import { createSupabaseServerClient } from '@/config/supabaseClient';
 
 export const dynamic = "force-dynamic";
 
